87454dd8fdfe109eb9803bdb66e87c8f92cff213
GiroSan <girosan86@gmail.com>
2023-07-09 15:38:33 -0300

    Estilização balão de erro

--- script.js
+++ script.js
@@ -1,7 +1,37 @@
-const fields = document.querySelectorAll("[required")
+const fields = document.querySelectorAll("[required]")
 
+function customValidation(event){
 
-console.log(fields)
+    event.preventDefault()
+    const field = event.target
+
+    function verifyError(){
+        let foundError = false
+
+        for (const error in field.validity){
+            if (error !== "customError" && field.validity[error]){
+                foundError =  true
+            }
+        }
+        
+        return foundError
+    }
+   
+    const error = verifyError()
+    console.log("Error exist: ", error)
+
+    if (error){
+        field.setCustomValidity("Esse campo é obrigatório")
+    } else {
+        field.setCustomValidity("")
+    }
+
+    
+}
+
+for (field of fields ){
+    field.addEventListener("invalid", customValidation)
+}
 
 
 
